04c0af699fb6be82eb204d815340a8193b53d2bb
Blazin-2180 <scotthoare89@gmail.com>
2025-10-03 22:10:16 +0100

    Player Spawner

--- Globals/global_player_manager.gd
+++ Globals/global_player_manager.gd
@@ -1,13 +1,36 @@
 extends Node
 
+const PLAYER = preload("uid://d3qi426qtce4j")
+
 var player : Player
 var health_points : int = 6
 var max_health_points : int = 6
+var player_spawned : bool = false
 
 func _ready() -> void:
+	add_player_instance()
+	await get_tree().create_timer(0.2).timeout
+	player_spawned = true
 	pass
 	
 func reduce_health_points ( num : int ) -> void :
 	health_points -= num
 	PlayerHud.update_health_points()
 	pass
+
+func add_player_instance () -> void :
+	player = PLAYER.instantiate()
+	add_child( player )
+	pass
+
+func set_player_position( _new_position : Vector2) -> void :
+	player.global_position = _new_position
+	pass
+
+func set_as_parent ( _p : Node2D ) -> void :
+	if player.get_parent ():
+		player.get_parent().remove_child( player )
+	_p.add_child( player )
+
+func unparent_player ( _p : Node2D ) -> void :
+	_p.remove_child( player )
---
+++ Scripts/level.gd
@@ -0,0 +1,7 @@
+class_name Level extends Node2D
+
+func _ready() -> void:
+	#self.y_sort_enabled = true
+	#self.z_as_relative = false
+	#GlobalPlayerManager.set_as_parent( self )
+	pass

784644cb12f12f25ac54fc622677051e86043721
Blazin-2180 <scotthoare89@gmail.com>
2025-10-04 14:26:46 +0100

    Saving is bringing up an error
    E 0:00:03:003   update_player_data: Invalid access to property or key 'health_points' on a base object of type 'CharacterBody2D (Player)'.
      <GDScript Source>global_save_manager.gd:36 @ update_player_data()
      <Stack Trace> global_save_manager.gd:36 @ update_player_data()
                    global_save_manager.gd:22 @ save_game()
                    pause_menu.gd:36 @ _on_save_pressed()

---
+++ Globals/global_save_manager.gd
@@ -0,0 +1,39 @@
+extends Node
+
+const SAVE_PATH = "user://"
+
+signal game_loaded
+signal game_saved
+
+var current_save : Dictionary = {
+	scene_path = "",
+	player = {
+		health_points = 1,
+		max_health_points = 1,
+		position_x = 0,
+		position_y = 0
+	},
+	items = [],
+	persistence = [],
+	quests = [],
+}
+
+func save_game() -> void :
+	update_player_data()
+	var file := FileAccess.open( SAVE_PATH + "save.sav", FileAccess.WRITE )
+	var save_json = JSON.stringify( current_save )
+	file.store_line( save_json )
+	game_saved.emit()
+	print("game saved")
+	pass
+
+func load_game () -> void: 
+	print("game loaded")
+	pass
+
+func update_player_data () -> void :
+	var p : Player = GlobalPlayerManager.player
+	current_save.player.health_points = p.health_points
+	current_save.player.max_health_points = p.max_health_points
+	current_save.player.position_x = p.global_position.x
+	current_save.player.position_y = p.global_position.y

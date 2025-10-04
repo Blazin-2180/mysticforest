ee0f7edf0f77a2c5341e5917bf9ebf64b15bb59a
Blazin-2180 <scotthoare89@gmail.com>
2025-10-04 13:37:25 +0100

    Camera Bounds Added

--- Globals/global_level_manager.gd
+++ Globals/global_level_manager.gd
@@ -1,10 +1,11 @@
 extends Node
 
-# Variables
+# Signals
 signal level_load_started
 signal level_loaded
 signal tilemap_bounds_changed (bounds : Array [ Vector2 ] )
 
+# Variables
 var current_tilemap_bounds : Array [ Vector2 ]
 var target_transition : String
 var position_offset : Vector2
@@ -13,7 +14,7 @@ func _ready() -> void:
 	await get_tree().process_frame
 	level_loaded.emit()
 	
-func ChangeTileMapBounds ( bounds : Array [ Vector2 ] ) -> void : 
+func change_tilemap_bounds ( bounds : Array [ Vector2 ] ) -> void : 
 	current_tilemap_bounds = bounds
 	tilemap_bounds_changed.emit( bounds )
 

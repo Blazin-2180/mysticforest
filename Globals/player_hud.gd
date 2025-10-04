53c58fd8eee027957ec318b7b091c641913b2452
Blazin-2180 <scotthoare89@gmail.com>
2025-10-04 21:51:31 +0100

    ...

--- Globals/player_hud.gd
+++ Globals/player_hud.gd
@@ -6,7 +6,7 @@ extends CanvasLayer
 @onready var death: Control = $Control/Death
 @onready var respawn_button: Button = $Control/Death/VBoxContainer/RespawnButton
 @onready var back_to_title_button: Button = $Control/Death/VBoxContainer/BackToTitleButton
-@onready var game_over: AnimationPlayer = $Control/Death/GameOverAnimation
+@onready var animation_player: AnimationPlayer = $Control/Death/GameOverAnimation
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,13 +24,13 @@ func update_health_points() -> void :
 	pass
 
 func show_death_screen() -> void :
-	game_over.visible = true
-	game_over.mouse_filter = Control.MOUSE_FILTER_STOP
-	game_over.play("show_death")
-	await game_over.animation_finished
+	death.visible = true
+	death.mouse_filter = Control.MOUSE_FILTER_STOP
+	animation_player.play("show_death")
+	await animation_player.animation_finished
 
 func hide_death_screen() -> void :
-	game_over.visible = false
-	game_over.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	game_over.modulate = Color(1,1,1, 0)
+	death.visible = false
+	death.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	death.modulate = Color(1,1,1, 0)
 	pass

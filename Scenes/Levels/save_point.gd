de2deb7111c92b273a2d970074628178561ff984
Blazin-2180 <scotthoare89@gmail.com>
2025-10-12 21:08:57 +0100

    save bug fix

--- Scenes/Levels/save_point.gd
+++ Scenes/Levels/save_point.gd
@@ -3,22 +3,33 @@ class_name SavePoint extends Area2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var game_saved: Label = $GameSaved
 @onready var timer: Timer = $GameSaved/Timer
+@onready var interact_area: Area2D = $Area2D
+
 
 var can_interact = false
 
 func _ready() -> void:
 	game_saved.visible = false
-	
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player" :
-		can_interact = true
-	Input.is_action_pressed("interact")
-	print("Save Point Touched")
-	pass # Replace with function body.
+	interact_area.area_entered.connect( _on_area_enter )
+	interact_area.area_exited.connect( _on_area_exit )
 
-func _on_body_exited(body : Node2D) -> void :
-	if body.name == "Player" :
-		can_interact = false
+func _on_area_enter( _a : Area2D ) -> void :
+	GlobalPlayerManager.interact_pressed.connect( interact )
+	pass
+
+func _on_area_exit( _a : Area2D ) -> void :
+	GlobalPlayerManager.interact_pressed.disconnect( interact )
+	pass
+	
+#func _on_body_entered(body: Node2D) -> void:
+	#if body.name == "Player" :
+		#can_interact = true
+	#Input.is_action_pressed("interact")
+	#pass # Replace with function body.
+#
+#func _on_body_exited(body : Node2D) -> void :
+	#if body.name == "Player" :
+		#can_interact = false
 
 func _process(_delta: float) -> void:
 	if can_interact &&  Input.is_action_pressed( "interact" ) :

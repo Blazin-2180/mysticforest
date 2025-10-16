382c21aef44340171aaf463d22d52d85b66044d3
Blazin-2180 <scotthoare89@gmail.com>
2025-10-16 17:49:19 +0100

    NPC dialog audio
    Would like to adapt this to voice acted.

--- GUI/dialog_system/scripts/dialog_system.gd
+++ GUI/dialog_system/scripts/dialog_system.gd
@@ -2,17 +2,31 @@
 #@icon()
 class_name DialogSystemNode extends CanvasLayer
 
+#region /// SIGNALS
 signal finished 
+signal letter_added( letter : String )
+#endregion
 
+#region /// STANDARD VARIABLES
 var is_active : bool = false
+var text_in_progress : bool = false
+var text_speed : float = 0.02
+var text_length : int = 0
+var plain_text : String
 var dialog_items : Array [ DialogItem ]
 var dialog_item_index : int = 0
+#endregion
 
+#region /// ON READY VARIABLES
 @onready var dialog_ui : Control = $DialogUI
 @onready var content : RichTextLabel = $DialogUI/TextureRect/PanelContainer/RichTextLabel
 @onready var name_label : Label = $DialogUI/NameLabel
 @onready var dialog_progress_indicator : PanelContainer = $DialogUI/DialogProgressIndicator
 @onready var progress_dialog : Label = $DialogUI/DialogProgressIndicator/Label
+@onready var timer: Timer = $DialogUI/Timer
+@onready var audio_stream_player: AudioStreamPlayer = $DialogUI/AudioStreamPlayer
+#@onready var portrait_sprite: Sprite2D = $DialogUI/PortraitSprite
+#endregion
 
 func _ready() -> void:
 	if Engine.is_editor_hint():
@@ -20,6 +34,7 @@ func _ready() -> void:
 			get_parent().remove_child( self )
 			return
 		return
+	timer.timeout.connect(_on_timer_timeout)
 	hide_dialog()
 
 func _unhandled_input( event : InputEvent ) -> void:
@@ -30,6 +45,12 @@ func _unhandled_input( event : InputEvent ) -> void:
 		event.is_action("attack") ||
 		event.is_action_pressed("ui_accept")
 	) :
+		if text_in_progress == true :
+			content.visible_characters = text_length
+			timer.stop()
+			text_in_progress = false
+			show_dialog_button_indicator( true )
+			return
 		dialog_item_index += 1
 		if dialog_item_index < dialog_items.size() :
 			start_dialog()
@@ -57,11 +78,27 @@ func hide_dialog() -> void :
 	pass
 
 func start_dialog() -> void :
-	show_dialog_button_indicator(true)
+	show_dialog_button_indicator( false )
 	var _d : DialogItem = dialog_items[dialog_item_index]
 	set_dialog_data( _d )
+	
+	content.visible_characters = 0
+	text_length = content.get_total_character_count()
+	plain_text = content.get_parsed_text()
+	text_in_progress = true
+	start_timer()
 	pass
 
+func _on_timer_timeout() -> void :
+	content.visible_characters += 1
+	if content.visible_characters <=text_length :
+		audio_stream_player.play()
+		start_timer()
+	else : 
+		show_dialog_button_indicator( true )
+		text_in_progress = false
+	pass
+	
 func set_dialog_data( _d : DialogItem) -> void :
 	if _d is DialogText :
 		content.text = _d.text
@@ -75,4 +112,10 @@ func show_dialog_button_indicator( _is_visible : bool) -> void :
 		progress_dialog.text = "Continue"
 	else :
 		progress_dialog.text = "Finish"
+	pass
+
+func start_timer() -> void :
+	timer.wait_time = text_speed
+	# Manipulate the wait_time
+	timer.start()
 	pass

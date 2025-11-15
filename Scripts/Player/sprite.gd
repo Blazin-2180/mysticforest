89ad40c8aa46ccc0e6e2503884fc248cfe391676
Scott <scotthoare89@gmail.com>
2025-11-15 00:06:00 +0000

    14/11 - 1
    Inventory update. Now able to swap out character equipment! :D

--- Scripts/Player/sprite.gd
+++ Scripts/Player/sprite.gd
@@ -2,39 +2,49 @@ extends Sprite2D
 
 const FRAME_COUNT : int = 10
 
-@export var head_sprite : Sprite2D
-@export var shoulder_sprite : Sprite2D
-@export var back_sprite : Sprite2D
-@export var hand_sprite : Sprite2D
-@export var chest_sprite : Sprite2D
-@export var legs_sprite : Sprite2D
-@export var main_hand_above : Sprite2D
-@export var main_hand_below : Sprite2D
-@export var feet_sprite : Sprite2D
-@export var offhand_above : Sprite2D
-@export var offhand_below : Sprite2D
+#@onready var head_sprite: Sprite2D = $head_sprite
+#@onready var shoulder_sprite: Sprite2D = $shoulder_sprite
+#@onready var back_sprite: Sprite2D = $back_sprite
+@onready var hands_sprite: Sprite2D = $hands_sprite
+@onready var chest_sprite: Sprite2D = $chest_sprite
+@onready var legs_sprite: Sprite2D = $legs_sprite
+@onready var feet_sprite: Sprite2D = $feet_sprite
+#@onready var mainhand_above_sprite: Sprite2D = $mainhand_above_sprite
+#@onready var mainhand_below_sprite: Sprite2D = $mainhand_below_sprite
+#@onready var offhand_above_sprite: Sprite2D = $offhand_above_sprite
+#@onready var offhand_below_sprite: Sprite2D = $offhand_below_sprite
 
 
 func _ready() -> void:
-	GlobalPlayerManager.INVENTORY_DATA.equipement_changed.connect( _on_equipment_changed )
+	GlobalPlayerManager.INVENTORY_DATA.equipment_changed.connect( _on_equipment_changed )
 	pass
 
 
 func _process( _delta : float ) -> void:
-	head_sprite.frame = frame
-	shoulder_sprite.frame = frame
-	back_sprite.frame = frame
-	hand_sprite.frame = frame
+	#head_sprite.frame = frame
+	#shoulder_sprite.frame = frame
+	#back_sprite.frame = frame
+	hands_sprite.frame = frame
 	chest_sprite.frame = frame
 	legs_sprite.frame = frame
-	main_hand_above.frame = frame + FRAME_COUNT
-	main_hand_below.frame = frame
+	#main_hand_above.frame = frame + FRAME_COUNT
+	#mainhand_below_sprite.frame = frame
 	feet_sprite.frame = frame
-	offhand_above.frame = frame + FRAME_COUNT
-	offhand_below.frame = frame
+	#offhand_above_sprite.frame = frame + FRAME_COUNT
+	#offhand_below_sprite.frame = frame
 	pass
 
 func _on_equipment_changed() -> void :
 	var equipment : Array[ SlotData ] = GlobalPlayerManager.INVENTORY_DATA.equipment_slots()
-	texture = equipment[ 0 ].item_data.sprite_texture
+	#head_sprite.texture = equipment[ 0 ].item_data.sprite_texture
+	#shoulder_sprite.texture = equipment[ 1 ].item_data.sprite_texture
+	#back_sprite.texture = equipment[ 3 ].item_data.sprite_texture
+	hands_sprite.texture = equipment[ 4 ].item_data.sprite_texture
+	chest_sprite.texture = equipment[ 5 ].item_data.sprite_texture
+	legs_sprite.texture = equipment[ 7 ].item_data.sprite_texture
+	feet_sprite.texture = equipment[10].item_data.sprite_texture
+	#main_hand_above.texture = equipment[ 9 ].item_data.sprite_texture
+	#mainhand_below_sprite.texture = equipment[ 9 ].item_data.sprite_texture
+	#offhand_above_sprite = equipment[ 11 ].item_data.sprite_texture
+	#offhand_below_sprite.texture = equipment[ 11 ].item_data.sprite_texture
 	pass
